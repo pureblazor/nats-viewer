cb3636e507446140ec5095e8f789db134d778bfb
Cody Mullins <joshua.c.mullins@gmail.com>
2024-04-02 19:43:47 -0400

    fix pathing

---
+++ src/NatsViewer/Program.cs
@@ -0,0 +1,40 @@
+using NatsViewer.Client;
+using NatsViewer.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpClient<NatsMonitorClient>(c =>
+{
+    c.BaseAddress = new Uri("http://localhost:8222");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(NatsViewer.Client._Imports).Assembly);
+
+app.Run();
